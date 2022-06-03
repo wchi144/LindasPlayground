5331f35b47a102b6b55d1d5ee340279a30b216be
wchi144 <linda.c218@gmail.com>
2022-06-03 16:44:18 +1200

    add gitignore

---
+++ StackAllocationOnly/ByteArrayToHexaBenchmark.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Text;
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)] // Order the result
+[RyuJitX64Job, LegacyJitX86Job] // Run with x64 and x86 runtimes
+[MemoryDiagnoser] // Analyse the memory usage
+public class ByteArrayToHexaBenchmark
+{
+    // Initialize the byte array for each run
+    private byte[] _array;
+
+    [Params(10, 1000, 10000)]
+    public int Size { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _array = Enumerable.Range(0, Size).Select(i => (byte)i).ToArray();
+    }
+
+    // Code to benchmark
+    [Benchmark(Baseline = true)]
+    public string ToHexWithStringBuilder() => ToHexWithStringBuilder(_array);
+
+    [Benchmark]
+    public string ToHexWithBitConverter() => ToHexWithBitConverter(_array);
+
+    public string ToHexWithStringBuilder(byte[] bytes)
+    {
+        var hex = new StringBuilder(bytes.Length * 2);
+        foreach (byte b in bytes)
+            hex.Append(b.ToString("X2"));
+        return hex.ToString();
+    }
+
+    public string ToHexWithBitConverter(byte[] bytes)
+    {
+        var hex = BitConverter.ToString(bytes);
+        return hex.Replace("-", "");
+    }
+}
---
+++ StackAllocationOnly/SplitStringNoHeapAllocation.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * using code from https://blog.ndepend.com/improve-c-code-performance-with-spant/
+*/
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)] // Order the result
+[RyuJitX64Job, LegacyJitX86Job] // Run with x64 and x86 runtimes
+[MemoryDiagnoser] // Analyse the memory usage
+public class SplitStringNoHeapAllocation
+{
+    //private string _input;
+    private const int numberOfElements = 7;
+    private const string intput = "1, 20, 35, 130, 3, 10, 160";
+    
+
+    [Params(10, 1000, 10000)]
+    public int Size { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        //_input = Enumerable.Range(0, Size).Select(i => (byte)i).ToArray();
+        //_input = "1, 20, 35, 130, 3, 10, 160";
+    }
+
+    // Code to benchmark
+    [Benchmark(Baseline = true)]
+    public int GetStringSumWithSpan() => GetStringSumWithSpan(intput);
+
+    [Benchmark]
+    public int GetStringSum() => GetStringSum(intput);
+
+    public int GetStringSum(string input)
+    {
+        var numbers = input.Split(",").Select(x => int.Parse(x));
+        var sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public int GetStringSumWithSpan(string input)
+    {
+        var spanOfNumber = SplitUsingSpan(input);
+        var sum = 0;
+        foreach (var span in spanOfNumber)
+        {
+            sum += span;
+        }
+        return sum;
+    }
+
+    public int[] SplitUsingSpan(string input)
+    {
+        var result = new int[numberOfElements];
+        ReadOnlySpan<char> spanChar = input.AsSpan();
+        var nextCommaIndex = 0;
+        var insertValueAtIndex = 0;
+        var isLastLoop = false;
+
+        while (!isLastLoop)
+        {
+            var indexStart = nextCommaIndex;
+            nextCommaIndex = input.IndexOf(',', indexStart);
+
+            isLastLoop = nextCommaIndex == -1;
+            if (isLastLoop)
+            {
+                nextCommaIndex = input.Length;
+            }
+            ReadOnlySpan<char> slice = spanChar.Slice(indexStart, nextCommaIndex - indexStart);
+            var value = int.Parse(slice);
+            result[insertValueAtIndex] = value;
+            insertValueAtIndex++;
+            nextCommaIndex++;
+        }
+        return result;
+    }
+}
